8cb8ecf3c33f964677d37979a5a6804ce1e8cdf9
mohdaltaf145 <altafshaikhmd6@gmail.com>
2022-08-22 03:28:42 +0530

    added api link

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -21,7 +21,8 @@
 
 import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/api/"
+// const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://mykart2.herokuapp.com/api/"
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTI1N2Q2ZWViMWRjYTBjNWFhNTcwNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MDQ4OTMwMywiZXhwIjoxNjUwNzQ4NTAzfQ.8sgOjw5-Aieo4kEgks5CPueogHmK7wA6oTD5yFawe4Q"
 
